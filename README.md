4c25b67b3e4f5944f1333e39d874c0b42f4451bc
Skye Doto <skye@doto.works>
2022-06-17 17:56:55 -0400

    Better

---
+++ README.md
@@ -0,0 +1,26 @@
+# BTA InfoPanel for OBS
+
+This project provides infopanes & a control panel for OBS web sources, allowing BTA info to be displayed *well* and certain things to be automated.
+
+## Running
+
+Install nodejs.
+
+`npm install`
+
+`npm start`
+
+It's that simple.
+
+## Using
+
+Navigate to `localhost:8080/ControlPanel` to control things
+
+Add any of the following pages as websources to OBS (`localhost:8080/InfoPane/<pane>`)
+| Pane        | Description                    |
+| ----------- | ------------------------------ |
+| `block`     | The chopping block             |
+| `division`  | The current division           |
+| `objective` | The current objective          |
+| `players`   | The current player list        |
+| `levels`    | Currently available level list |
---
+++ Translator.js
@@ -0,0 +1,418 @@
+//const data = [
+//  [
+//    {
+//      "name": "All Berries in Start of 1A (6)",
+//      "types": [
+//        "checkpoint_berries"
+//      ]
+//    },
+//    {
+//      "name": "Old Site Blue Heart",
+//      "types": [
+//        "earlygame",
+//        "blue_heart"
+//      ]
+//    },
+//    {
+//      "name": "Complete 1A Start without dashing",
+//      "types": [
+//        "earlygame",
+//        "task"
+//      ]
+//    },
+//    {
+//      "name": "Forsaken City Cassette",
+//      "types": [
+//        "earlygame",
+//        "cassette"
+//      ]
+//    },
+//    {
+//      "name": "Forsaken City Blue Heart",
+//      "types": [
+//        "earlygame",
+//        "blue_heart"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "All Berries in Awake (1)",
+//      "types": [
+//        "earlygame"
+//      ]
+//    },
+//    {
+//      "name": "Old Site Cassette",
+//      "types": [
+//        "earlygame",
+//        "cassette"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Read the Poem in 2A",
+//      "types": [
+//        "earlygame",
+//        "awake"
+//      ]
+//    },
+//  ],
+//  [
+//    {
+//      "name": "Old Site B-Side",
+//      "types": [
+//        "b-side"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Forsaken City B-Side",
+//      "types": [
+//        "b-side"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Huge Mess: Chest -\u003E Books -\u003E Towel",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    },
+//    {
+//      "name": "Celestial Resort Blue Heart",
+//      "types": [
+//        "blue_heart"
+//      ]
+//    },
+//    {
+//      "name": "Huge Mess: Chest -\u003E Towel -\u003E Books",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    },
+//    {
+//      "name": "Huge Mess: Towel -\u003E Books -\u003E Chest",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Golden Ridge Cassette",
+//      "types": [
+//        "cassette",
+//        "midgame"
+//      ]
+//    },
+//    {
+//      "name": "Golden Ridge Blue Heart",
+//      "types": [
+//        "blue_heart",
+//        "midgame"
+//      ]
+//    },
+//  ],
+//  [
+//    {
+//      "name": "Huge Mess: Books -\u003E Towel -\u003E Chest",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    },
+//    {
+//      "name": "Huge Mess: Books -\u003E Chest -\u003E Towel",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    },
+//    {
+//      "name": "Huge Mess: Towel -\u003E Chest -\u003E Books",
+//      "types": [
+//        "huge_mess_pathing"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Celestial Resort Cassette",
+//      "types": [
+//        "cassette"
+//      ]
+//    },
+//    {
+//      "name": "Mirror Temple Cassette",
+//      "types": [
+//        "cassette",
+//        "progress"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Golden Ridge B-Side",
+//      "types": [
+//        "b-side"
+//      ]
+//    },
+//    {
+//      "name": "Mirror Temple B-Side",
+//      "types": [
+//        "5b",
+//        "progress"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Mirror Temple Blue Heart",
+//      "types": [
+//        "blue_heart"
+//      ]
+//    },
+//    {
+//      "name": "Hit a Kevin block from all 4 sides",
+//      "types": [
+//        "task",
+//        "progress"
+//      ]
+//    },
+//    {
+//      "name": "All Berries in Unraveling (1)",
+//      "types": [
+//        "5a"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "All Berries in Rescue (1)",
+//      "types": [
+//        "5a"
+//      ]
+//    },
+//    {
+//      "name": "All Berries in Into the Core (1)",
+//      "types": [
+//        "core"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Get 1 Key in Search",
+//      "types": [
+//        "5a",
+//        "keys"
+//      ]
+//    },
+//    {
+//      "name": "Reflection Cassette",
+//      "types": [
+//        "cassette",
+//        "progress"
+//      ]
+//    },
+//  ],
+//  [
+//    {
+//      "name": "Mirror Temple A-Side",
+//      "types": [
+//        "5a"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Get 2 Keys in Search",
+//      "types": [
+//        "5a",
+//        "keys"
+//      ]
+//    },
+//    {
+//      "name": "Celestial Resort B-Side",
+//      "types": [
+//        "3b"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Easteregg Room in Reflection",
+//      "types": [
+//        "task",
+//        "6a"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "3 Gems in The Summit",
+//      "types": [
+//        "gems"
+//      ]
+//    },
+//    {
+//      "name": "0M and 500M Gems",
+//      "types": [
+//        "gems",
+//        "progress"
+//      ]
+//    },
+//    {
+//      "name": "Reflection A-Side",
+//      "types": [
+//        "lategame",
+//        "6a"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "The Summit Cassette",
+//      "types": [
+//        "cassette"
+//      ]
+//    },
+//    {
+//      "name": "1000M and 1500M Gems",
+//      "types": [
+//        "gems",
+//        "lategame"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "Reflection B-Side",
+//      "types": [
+//        "6b"
+//      ]
+//    },
+//    {
+//      "name": "2000M and 2500M Gems",
+//      "types": [
+//        "gems",
+//        "lategame"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "All Berries in Heart of the Mountain (1)",
+//      "types": [
+//        "core"
+//      ]
+//    },
+//    {
+//      "name": "Core Blue Heart",
+//      "types": [
+//        "core"
+//      ]
+//    }
+//  ],
+//  [
+//    {
+//      "name": "The Summit B-Side",
+//      "types": [
+//        "7b"
+//      ]
+//    }
+//  ]
+//]
+//
+//function ArrayAddRange(target, source){
+//    if(Array.isArray(source)){
+//        for(let sr of source){
+//            ArrayAddRange(target, sr)
+//        }
+//    }
+//    else{
+//        target.push(source);
+//    }
+//}
+//
+//let arr = [];
+//ArrayAddRange(arr, data);
+//console.log(JSON.stringify(arr));
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+//const data = JSON.parse('[{"name":"All Berries in Start of 1A (6)","types":["checkpoint_berries"]},{"name":"Old Site Blue Heart","types":["earlygame","blue_heart"]},{"name":"Complete 1A Start without dashing","types":["earlygame","task"]},{"name":"Forsaken City Cassette","types":["earlygame","cassette"]},{"name":"Forsaken City Blue Heart","types":["earlygame","blue_heart"]},{"name":"All Berries in Awake (1)","types":["earlygame"]},{"name":"Old Site Cassette","types":["earlygame","cassette"]},{"name":"Read the Poem in 2A","types":["earlygame","awake"]},{"name":"Old Site B-Side","types":["b-side"]},{"name":"Forsaken City B-Side","types":["b-side"]},{"name":"Huge Mess: Chest -> Books -> Towel","types":["huge_mess_pathing"]},{"name":"Celestial Resort Blue Heart","types":["blue_heart"]},{"name":"Huge Mess: Chest -> Towel -> Books","types":["huge_mess_pathing"]},{"name":"Huge Mess: Towel -> Books -> Chest","types":["huge_mess_pathing"]},{"name":"Golden Ridge Cassette","types":["cassette","midgame"]},{"name":"Golden Ridge Blue Heart","types":["blue_heart","midgame"]},{"name":"Huge Mess: Books -> Towel -> Chest","types":["huge_mess_pathing"]},{"name":"Huge Mess: Books -> Chest -> Towel","types":["huge_mess_pathing"]},{"name":"Huge Mess: Towel -> Chest -> Books","types":["huge_mess_pathing"]},{"name":"Celestial Resort Cassette","types":["cassette"]},{"name":"Mirror Temple Cassette","types":["cassette","progress"]},{"name":"Golden Ridge B-Side","types":["b-side"]},{"name":"Mirror Temple B-Side","types":["5b","progress"]},{"name":"Mirror Temple Blue Heart","types":["blue_heart"]},{"name":"Hit a Kevin block from all 4 sides","types":["task","progress"]},{"name":"All Berries in Unraveling (1)","types":["5a"]},{"name":"All Berries in Rescue (1)","types":["5a"]},{"name":"All Berries in Into the Core (1)","types":["core"]},{"name":"Get 1 Key in Search","types":["5a","keys"]},{"name":"Reflection Cassette","types":["cassette","progress"]},{"name":"Mirror Temple A-Side","types":["5a"]},{"name":"Get 2 Keys in Search","types":["5a","keys"]},{"name":"Celestial Resort B-Side","types":["3b"]},{"name":"Easteregg Room in Reflection","types":["task","6a"]},{"name":"3 Gems in The Summit","types":["gems"]},{"name":"0M and 500M Gems","types":["gems","progress"]},{"name":"Reflection A-Side","types":["lategame","6a"]},{"name":"The Summit Cassette","types":["cassette"]},{"name":"1000M and 1500M Gems","types":["gems","lategame"]},{"name":"Reflection B-Side","types":["6b"]},{"name":"2000M and 2500M Gems","types":["gems","lategame"]},{"name":"All Berries in Heart of the Mountain (1)","types":["core"]},{"name":"Core Blue Heart","types":["core"]},{"name":"The Summit B-Side","types":["7b"]}]')
+const data = JSON.parse(' [ { "name": "All Berries in Start of 1A (6)" }, { "name": "Old Site Blue Heart" }, { "name": "Complete 1A Start without dashing" }, { "name": "Forsaken City Cassette" }, { "name": "Forsaken City Blue Heart" }, { "name": "All Berries in Awake (1)" }, { "name": "Old Site Cassette" }, { "name": "Read the Poem in 2A" }, { "name": "Old Site B-Side" }, { "name": "Forsaken City B-Side" }, { "name": "Huge Mess: Chest -\u003E Books -\u003E Towel" }, { "name": "Celestial Resort Blue Heart" }, { "name": "Huge Mess: Chest -\u003E Towel -\u003E Books" }, { "name": "Huge Mess: Towel -\u003E Books -\u003E Chest" }, { "name": "Golden Ridge Cassette" }, { "name": "Golden Ridge Blue Heart" }, { "name": "Huge Mess: Books -\u003E Towel -\u003E Chest" }, { "name": "Huge Mess: Books -\u003E Chest -\u003E Towel" }, { "name": "Huge Mess: Towel -\u003E Chest -\u003E Books" }, { "name": "Celestial Resort Cassette" }, { "name": "Mirror Temple Cassette" }, { "name": "Golden Ridge B-Side" }, { "name": "Mirror Temple B-Side" }, { "name": "Mirror Temple Blue Heart" }, { "name": "Hit a Kevin block from all 4 sides" }, { "name": "All Berries in Unraveling (1)" }, { "name": "All Berries in Rescue (1)" }, { "name": "All Berries in Into the Core (1)" }, { "name": "Get 1 Key in Search" }, { "name": "Reflection Cassette" }, { "name": "Mirror Temple A-Side" }, { "name": "Get 2 Keys in Search" }, { "name": "Celestial Resort B-Side" }, { "name": "Easteregg Room in Reflection" }, { "name": "3 Gems in The Summit" }, { "name": "0M and 500M Gems" }, { "name": "Reflection A-Side" }, { "name": "The Summit Cassette" }, { "name": "1000M and 1500M Gems" }, { "name": "Reflection B-Side" }, { "name": "2000M and 2500M Gems" }, { "name": "All Berries in Heart of the Mountain (1)" }, { "name": "Core Blue Heart" }, { "name": "The Summit B-Side" } ]');
+
+let finalData = {}
+
+function readLineAsync(message) {
+    return new Promise((resolve, reject) => {
+        readline.question(message, (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+async function DoDataThings() {
+    let finalList = {'1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': []};
+    readline.on('SIGINT', () => {
+        process.emit('SIGINT');
+    })
+    process.on('SIGINT', () => {
+        console.log('INTERRUPTED! Dumping List');
+        console.log(JSON.stringify(finalList));
+        process.exit();
+    })
+    for (let obj of data) {
+        let finalObj = {};
+        console.log(obj);
+
+        let input = "";
+        input = await(readLineAsync("New Title: "));
+        finalObj['title'] = input || obj.name;
+
+        input = "%";
+        while (!input.match(/[1-8 ]+/)) {
+            input = await readLineAsync("List of chapters this applies to: ");
+        }
+        let chapters = new Set(input.replace(/ /g, '').split(''));
+        finalObj['sides'] = {};
+        for(let chap of chapters){
+            input = "%";
+            while(!input.match(/[a-c ]+$/)){
+                input = await(readLineAsync(`Sides for chapter ${chap}: `));
+            }
+            let sides = Array.from(new Set(input.replace(/ /g, '').split('')));
+            finalObj.sides[chap] = sides;
+        }
+
+        input = "%";
+        while(!input.match(/^[yn]$/)){
+            input = await(readLineAsync("Is this entry final (y/n)? "));
+        }
+        finalObj['is_final'] = input == 'y';
+    
+        for(let chap of chapters){
+            finalList[chap].push(finalObj);
+        }
+    }
+    console.log(JSON.stringify(finalList))
+}
+
+//DoDataThings()
+
+const fs = require('fs');
+let objs = JSON.parse(fs.readFileSync('objectives.json'));
+
+for(let chap in objs){
+    console.log(`__Chapter ${chap}__`)
+    for(let obj of objs[chap]){
+        console.log(` - ${obj.title}`)
+    }
+}
