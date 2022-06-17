d4cde7c6c11d234e4d2040e89a349ca57e8e5c15
Skye Doto <skye@doto.works>
2022-06-16 23:07:43 -0400

    Colors

--- Clients/InfoPane/block/btaMessageHandler.js
+++ Clients/InfoPane/block/btaMessageHandler.js
@@ -16,15 +16,23 @@ x.addEventListener('message', function (event) {
 
 function updateState(newState){
     let loser = document.getElementById('loser')
+    let blocktext = document.getElementById('blocktext')
     loser.innerHTML = "";
     if(newState.loser >= 0 && newState.players.length > newState.loser){
         let p = document.createElement('p');
-        p.innerText = newState.players[newState.loser];
-        if(newState.points[newState.players[newState.loser]] && newState.points[newState.players[newState.loser]] > 0)
-            p.innerText += ` (+${newState.points[newState.players[newState.loser]] * 5} secs)`
-        if(newState.points[newState.players[newState.loser]] && newState.points[newState.players[newState.loser]] < 0)
-            p.innerText += ` (${newState.points[newState.players[newState.loser]] * 5} secs)`
+        let player = newState.players[newState.loser];
+        p.innerText = player;
+        if(newState.points[player] && newState.points[player] > 0)
+            p.innerText += ` (+${newState.points[player] * 5} secs)`
+        if(newState.points[player] && newState.points[player] < 0)
+            p.innerText += ` (${newState.points[player] * 5} secs)`
         loser.appendChild(p);
+        if(player === "PRELIMS" || player === "FINALS"){
+            blocktext.innerText = "ㅤ";
+        }
+        else{
+            blocktext.innerText = "On the Block";
+        }
     }
     state = newState;
 }
